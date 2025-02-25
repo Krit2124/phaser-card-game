dfbb9eea856f98b4bd6134404d267925a5de07bd
Krit2124 <kritserg2124@gmail.com>
2025-02-25 22:20:15 +0300

    Initial commit

---
+++ src/app/main.tsx
@@ -0,0 +1,21 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout/MainLayout.tsx";
+import ErrorPage from "@/pages/ErrorPage/ErrorPage.tsx";
+import GameSelectionPage from "@/pages/GameSelectionPage/GameSelectionPage.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    errorElement: <ErrorPage />,
+    children: [{ path: "", element: <GameSelectionPage /> }],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
---
+++ src/pages/GameSelectionPage/GameSelectionPage.tsx
@@ -0,0 +1,9 @@
+const GameSelectionPage = () => {
+  return (
+    <div>
+      <h1>Здесь можно будет выбрать игру</h1>
+    </div>
+  );
+};
+
+export default GameSelectionPage;
